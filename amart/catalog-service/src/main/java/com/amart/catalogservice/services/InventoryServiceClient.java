7d298bbc193c4cdbb352621d81fbbae3381601fb
Tarun Sharma <tarun.sharma@newgen.co.in>
2019-06-23 11:26:24 +0530

    used @HystrixCommand to handle fallback of inventory-service

--- amart/catalog-service/src/main/java/com/amart/catalogservice/services/InventoryServiceClient.java
+++ amart/catalog-service/src/main/java/com/amart/catalogservice/services/InventoryServiceClient.java
@@ -1,6 +1,9 @@
 package com.amart.catalogservice.services;
 
 import com.amart.catalogservice.domain.InventoryItem;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
+import com.netflix.ribbon.proxy.annotation.Hystrix;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.netflix.eureka.http.RestTemplateTransportClientFactories;
@@ -10,6 +13,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
+import javax.persistence.criteria.CriteriaBuilder;
 import java.text.MessageFormat;
 import java.util.Optional;
 
@@ -22,20 +26,35 @@ public class InventoryServiceClient {
     @Autowired
     private RestTemplate restTemplate;
 
-    public Optional<InventoryItem> getProductInventoryByCode(String productCode) {
-        try {
-            ResponseEntity<InventoryItem> itemResponseEntity =
-                    restTemplate.getForEntity(INVENTORY_SERVICE_API_INVENTORY_PRODUCT_CODE,
-                            InventoryItem.class,
-                            productCode);
-            if (itemResponseEntity.getStatusCode() == HttpStatus.OK) {
-                return Optional.ofNullable(itemResponseEntity.getBody());
-            } else {
-                log.error("Unable to get inventory level for product_code: " + productCode + ", StatusCode: " + itemResponseEntity.getStatusCode());
+    @HystrixCommand(fallbackMethod = "getDefaultProductInventoryByCode",
+            commandProperties = {
+                    @HystrixProperty(name = "execution.isolation.thread.timeoutInMilliseconds", value = "3000"),
+                    @HystrixProperty(name = "circuitBreaker.errorThresholdPercentage", value = "60")
             }
-        } catch (HttpClientErrorException e) {
-            log.error("Unable to get inventory level for product_code: " + productCode, e);
+    )
+    public Optional<InventoryItem> getProductInventoryByCode(String productCode) {
+//        try {
+        ResponseEntity<InventoryItem> itemResponseEntity =
+                restTemplate.getForEntity(INVENTORY_SERVICE_API_INVENTORY_PRODUCT_CODE,
+                        InventoryItem.class,
+                        productCode);
+        if (itemResponseEntity.getStatusCode() == HttpStatus.OK) {
+            return Optional.ofNullable(itemResponseEntity.getBody());
+        } else {
+            log.error("Unable to get inventory level for product_code: " + productCode + ", StatusCode: " + itemResponseEntity.getStatusCode());
+            return Optional.empty();
         }
-        return Optional.empty();
+//        } catch (HttpClientErrorException e) {
+//            log.error("Unable to get inventory level for product_code: " + productCode, e);
+//        }
+//        return Optional.empty();
+    }
+
+    public Optional<InventoryItem> getDefaultProductInventoryByCode(String productCode) {
+        log.info("Returning default InventoryItem for productCode: " + productCode);
+        InventoryItem response = new InventoryItem();
+        response.setProductCode(productCode);
+        response.setAvailableQuantity(0);
+        return Optional.ofNullable(response);
     }
 }
